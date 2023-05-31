72b3d3a306c41a684d60f3cbbf6bb1edd49ccb0d
RKU-02 <rajubhadiya999@gmail.com>
2023-05-31 10:35:16 +0530

    add css of analyse page

--- src/cmp/Analyse.js
+++ src/cmp/Analyse.js
@@ -1,14 +1,52 @@
 import React from "react";
+import "./main.css";
+import AnalysecardList from "./AnalysecardList";
 
 function Analyse() {
-  return <div className="AnalysePage">
-    <div className="Analyseheader"><h2>Analyse</h2></div>
-    <div className="analsyscards">
-      <div className="cards">
-        
+  const AnalysecardsData = [
+    {
+      id: 1,
+      title: "breakfast",
+      image: "doenload.jpeg",
+    },
+    {
+      id: 2,
+      title: "Lunch",
+      image: "doenload.jpeg",
+    },
+    {
+      id: 3,
+      title: "Dinner",
+      image: "doenload.jpeg",
+    },
+    {
+      id: 4,
+      title: "Acadamic",
+      image: "doenload.jpeg",
+    },
+    {
+      id: 5,
+      title: "Grocery",
+      image: "doenload.jpeg",
+    },
+
+    {
+      id: 6,
+      title: "Miscellaneous",
+      image: "doenload.jpeg",
+    },
+  ];
+  return (
+    <div className="AnalysePage">
+      <div className="Analyseheader">
+        <h2>Analyse</h2>
       </div>
+      <div className="Analysecardlist">
+        <AnalysecardList AnalysecardsData={AnalysecardsData} />
+      </div>
+      <div className="Analysebutton"><button>+</button></div>
     </div>
-  </div>;
+  );
 }
 
 export default Analyse;
